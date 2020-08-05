2b71db4c5bea9aa9008351ae5045fb4e604d026a
Filipcl <filip.larsen@live.no>
2020-08-05 10:45:08 +0200

    fix(iFrame): fix in iframe from GA

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -15,30 +15,33 @@ function Carousel() {
       <Slider>
         <Slide className="slide" index={0}>
           <iframe
+            title="1"
             width="750"
             height="464"
             seamless
-            frameborder="0"
+            frameBorder="0"
             scrolling="no"
             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQerZOGkM4-4slIOiDVLD4fJbtU_13a4O0A75ZBPXlWLbPMjhtMkVCNfQCjU5jO3dE_secXVU4bSG6q/pubchart?oid=171757654&amp;format=interactive"
           ></iframe>
         </Slide>
         <Slide className="slide" index={1}>
           <iframe
+            title="2"
             width="757"
             height="464"
             seamless
-            frameborder="0"
+            frameBorder="0"
             scrolling="no"
             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQerZOGkM4-4slIOiDVLD4fJbtU_13a4O0A75ZBPXlWLbPMjhtMkVCNfQCjU5jO3dE_secXVU4bSG6q/pubchart?oid=809200848&amp;format=interactive"
           ></iframe>
         </Slide>
         <Slide className="slide" index={2}>
           <iframe
+            title="3"
             width="600"
             height="371"
             seamless
-            frameborder="0"
+            frameBorder="0"
             scrolling="no"
             src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQerZOGkM4-4slIOiDVLD4fJbtU_13a4O0A75ZBPXlWLbPMjhtMkVCNfQCjU5jO3dE_secXVU4bSG6q/pubchart?oid=1498342867&amp;format=interactive"
           ></iframe>
